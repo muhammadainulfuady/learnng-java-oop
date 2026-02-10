adae9706e6f054e87ff298982a7ab83851eb933f
Muhammad Ainul Fuady <ainulfuadi1234@gmail.com>
2026-02-11 00:22:42 +0700

    sekalian belajar cpp

---
+++ cpp/hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>  // Header file untuk input/output
+using namespace std; // Agar tidak perlu menulis std::cout
+
+int main()
+{
+    cout << "Halo nama saya adi" << endl;
+    // Menampilkan output ke layar
+    cout << "Halo Dunia!" << endl;
+    cout << "Belajar C++ Dasar" << endl;
+
+    int umur = 20; // Deklarasi variabel
+    cout << "Umur saya: " << umur << " tahun" << endl;
+
+    return 0; // Menandakan program selesai dengan sukses
+}
